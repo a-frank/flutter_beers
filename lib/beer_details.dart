988da9fb890dec8aafebb13b981c1f75bf71a7f1
Alexander Frank <alexander.frank@arconsis.com>
2018-03-12 16:13:23 +0100

    Added a scrollview to details for landscape

--- lib/beer_details.dart
+++ lib/beer_details.dart
@@ -1,6 +1,7 @@
 import 'dart:collection';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_app/i8n/BeerLocalization.dart';
 import 'package:flutter_app/net/beer_data.dart';
@@ -38,53 +39,54 @@ class BeerDetailsPage extends StatelessWidget {
 				),
 				body: new Padding(
 					padding: const EdgeInsets.all(16.0),
-					child: new Column(
-						children: <Widget>[
-							new Row(children: <Widget>[
-								new Image(image: new NetworkImage(beer.imageUrl), width: 150.0, height: 150.0),
-								new Expanded(
-										child: new FittedBox(
-												fit: BoxFit.fitWidth,
-												child: new Column(
-														crossAxisAlignment: CrossAxisAlignment.start,
-														mainAxisSize: MainAxisSize.min,
-														children: <Widget>[
-															new Text(
-																	beer.name,
-																	style: Theme
-																			.of(context)
-																			.textTheme
-																			.title,
-																	softWrap: true,
-																	maxLines: 1,
-																	overflow: TextOverflow.ellipsis
-															),
-															new Text(beer.tagLine,
-																	style: Theme
-																			.of(context)
-																			.textTheme
-																			.subhead)
-														])
+					child: new SingleChildScrollView(
+							child: new Column(
+								children: <Widget>[
+									new Row(children: <Widget>[
+										new Image(image: new NetworkImage(beer.imageUrl), width: 150.0, height: 150.0),
+										new Expanded(
+												child: new FittedBox(
+														fit: BoxFit.fitWidth,
+														child: new Column(
+																crossAxisAlignment: CrossAxisAlignment.start,
+																mainAxisSize: MainAxisSize.min,
+																children: <Widget>[
+																	new Text(
+																			beer.name,
+																			style: Theme
+																					.of(context)
+																					.textTheme
+																					.title,
+																			softWrap: true,
+																			maxLines: 1,
+																			overflow: TextOverflow.ellipsis
+																	),
+																	new Text(beer.tagLine,
+																			style: Theme
+																					.of(context)
+																					.textTheme
+																					.subhead)
+																])
+												)
 										)
-								)
-
-							]),
-							new Container(height: 25.0),
-							new Text(beer.description),
-							new Container(height: 25.0),
-							new Text(
-									BeerLocalization
-											.of(context)
-											.foodPairings,
-									style: new TextStyle(fontWeight: FontWeight.bold)
-							),
-							new Container(height: 10.0),
-							new ListView.builder(
-									shrinkWrap: true,
-									itemCount: beer.fooPairing.length,
-									itemBuilder: _foodPairingItem
+									]),
+									new Container(height: 25.0),
+									new Text(beer.description),
+									new Container(height: 25.0),
+									new Text(
+											BeerLocalization
+													.of(context)
+													.foodPairings,
+											style: new TextStyle(fontWeight: FontWeight.bold)
+									),
+									new Container(height: 10.0),
+									new ListView.builder(
+											shrinkWrap: true,
+											itemCount: beer.fooPairing.length,
+											itemBuilder: _foodPairingItem
+									)
+								],
 							)
-						],
 					),
 				));
 	}
